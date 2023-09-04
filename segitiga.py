45ec7eabd016b35e857600d4544ec6b8472c04c9
Calon Jenazah <126953090+ryasya@users.noreply.github.com>
2023-09-04 09:52:01 +0700

    Create segitiga.py

---
+++ segitiga.py
@@ -0,0 +1,8 @@
+print('##  Program Python Menghitung Luas Segitiga  ##')
+print('==============================================')
+print()
+  
+alas_segitiga = float(input('Input alas segitiga: '))
+tinggi_segitiga = float(input('Input tinggi segitiga: '))
+luas = (alas_segitiga * tinggi_segitiga)/2
+print('Luas segitiga = ',(luas))
